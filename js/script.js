68b6e2507bf7527fd0b1de7143ae958f5503321f
FelipeGB98 <Felipe.Bernardes@edu.pucrs.br>
2023-09-10 03:17:55 -0300

    desenvolvimento da troca de idiomas e ajuste na secao de contact para aparecer o footer

--- js/script.js
+++ js/script.js
@@ -39,89 +39,18 @@ window.onscroll = () => {
 }
 
 
-    // language selector
-
-    // const langEl = document.querySelector('language-selector');    
-    // const link = document.querySelectorAll('a');
-    // const about = document.querySelector('about');
-    // // const descrEl = document.querySelector('language-selector');   
-
-    // link.forEach(el => {
-    //     el.addEventListener('click', () => {
-    //         langEl.querySelector('.active').classList.remove('active');
-    //         el.classList.add('active');
-
-    //         const attr = el.getAttribute('language');
-
-    //         about.textContent = data[attr].about;
-    //     });
-    // });
-
-
-    // var data = {
-    //     "english":
-    //     {
-    //         "about": "About"
-    //     },
-    //     "portuguese":
-    //     {
-    //         "about": "Sobre"
-    //     }
-    // }
-
-
-
     // Mapeamento de traduções por idioma
-const translations = {
-    portuguese: {
-        logo: "Felipe"
-        // description: "Este é um exemplo simples de troca de idioma."
-    },
-    english: {
-        logo: "Philipe"
-        // description: "This is a simple language switcher example."
-    }
-};
+    function toggleDivVisibility(language) {
+        const divEnglish = document.getElementById('divEnglish');
+        
+        const divPortuguese = document.getElementById('divPortuguese');
 
-// Função para alterar o idioma da página
-function changeLanguage(language) {
-    // Obter todos os elementos com o atributo data-translate
-    const elementsToTranslate = document.querySelectorAll('[data-translate]');
+        if (language === 'portuguese') {
+            divEnglish.style.display = 'none';
+            divPortuguese.style.display = 'block'; 
+        } else if (language === 'english') {
+            divPortuguese.style.display = 'none';
+            divEnglish.style.display = 'block';
 
-    // Atualizar o conteúdo de cada elemento de acordo com o idioma selecionado
-    elementsToTranslate.forEach(element => {
-        const key = element.getAttribute('data-translate');
-        if (translations[language] && translations[language][key]) {
-            element.textContent = translations[language][key];
         }
-    });
-
-    // Remover a classe 'active' de todos os links de idioma
-    document.querySelectorAll('.language-selector a').forEach(link => {
-        link.classList.remove('active');
-    });
-
-    // Adicionar a classe 'active' ao link do idioma selecionado
-    document.querySelector(`[language="${language}"]`).classList.add('active');
-}
-
-// Adicionar manipuladores de evento de clique aos links de idioma
-document.querySelectorAll('.language-selector a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        const language = this.getAttribute('language');
-        changeLanguage(language);
-    });
-});
-
-// Adicionar manipuladores de evento de clique às imagens de bandeira
-document.querySelectorAll('.language-selector img').forEach(img => {
-    img.addEventListener('click', function (e) {
-        e.preventDefault();
-        const language = this.parentNode.getAttribute('language');
-        changeLanguage(language);
-    });
-});
-
-// Definir o idioma padrão ao carregar a página
-changeLanguage('english');
+    }
